47e2cbc40a2ff5901a521e0a4378fbae30adbbad
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-03 16:26:32 -0300

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('ola')
+import express from 'express'
+import { router } from './routes/routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+app.listen(4444, () => console.log('server is on 4444 port!'))
