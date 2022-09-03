5533fce27e950f7692c68ca1082b966f6a0f3d3f
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-03 16:28:18 -0300

    feat: users routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { userRouter } from "./User/UserRoutes";
 
 const router = Router()
 
 router.get('/', (request, response) => {
     return response.status(200).json({messsage: 'ok'})
 })
+router.use('/admin/users', userRouter)
 
 export {router}
