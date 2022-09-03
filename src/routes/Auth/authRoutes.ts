5e85aa7135ff8822845358b15ed07ff022216d4e
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-03 16:49:57 -0300

    feat: add login route

--- src/routes/Auth/authRoutes.ts
+++ src/routes/Auth/authRoutes.ts
@@ -14,5 +14,19 @@ authRouter.post("/register", async (request, response) => {
 
   return response.status(200).json({user})
 });
+authRouter.get('/login', async (request, response) => {
+    const {
+        email,
+        password
+      }: { email: string; password: string } = request.body;
+
+    const userExists = await UserRepository.findByEmail({email: email})
+
+    if (!userExists) return response.status(400).json({message: 'email or password invalids!'})
+
+    if (!(userExists.password === password)) return response.status(400).json({message: 'email or password invalids!'})
+
+    return response.status(200).json({message: 'ok'})
+})
 
 export { authRouter };
